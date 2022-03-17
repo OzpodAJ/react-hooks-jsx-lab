66f192bf45675f2a397ba6d5823930c7650ff41e
Andrew Osborn <osbornandrewj@gmail.com>
2022-03-17 15:36:20 -0700

    commit tree 4cbb09fcdc6a107b3bcb636abf64b5b87bd6d93a

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id = "about">
+    <h2>About Me</h2>
+      <p>Sometimes there are demons at the foot of my bed playing scrabble.</p>
+      <img src={image} alt="I made this"/>
+  </div>;
 }
 
 export default About;
